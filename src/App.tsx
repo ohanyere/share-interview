91d5eee193c64db1761239bc97f03a0368056d11
Ohanyere Francis <mooref068@gmail.com>
2025-09-04 08:25:28 -0700

    feat(routes) adds routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Toaster } from "react-hot-toast";
+import SignUp from "./pages/sign-up";
+import SignIn from "./pages/sign-in";
+import NotFound from "./pages/not-found";
+import Home from "./pages/home";
+import ForgetPassword from "./pages/forgot-password";
+import Navigation from "./components/navigation";
+import PrivateRoute from "./components/private-route";
+import ContributeData from "./pages/contribute-data";
+import Questions from "./pages/questions";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+
+
+
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-full">
+    <BrowserRouter>
+    <Navigation />
+      <Routes>
+        
+      <Route path="/" element={<Home/>} />
+      <Route path="/sign-up" element={<SignUp />} />
+     <Route path="/sign-in" element={<SignIn />} />
+     <Route path="/password-reset" element={<ForgetPassword/>} />
+     <Route path="/contribute-data" element={<ContributeData />} >
+     </Route>
+     <Route path="/view" element={<Questions />} />
+     <Route path="*" element={<NotFound />} /> 
+     {/* <Route path="/profile" element={<Privateroute />} >
+        <Route path="/profile" element={<StartupProfilePage />} />
+     </Route>
+     
+     
+{/* 
+     <Route path="/upload" element={<Upload />} />
+     <Route path="/result" element={<Result />} />
+     <Route path="/history" element={<History />} />
+    
+
+      */}
+      </Routes>
+    </BrowserRouter>
+    <Toaster />
+    </div>
   )
 }
 
 export default App
+
