3be4199a0f68b3f36eb9529593a3405945728826
Ohanyere Francis <mooref068@gmail.com>
2025-09-20 02:17:29 -0700

    Fix fallback route for Express v5

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 import { PORT, GENAI_API_KEY } from "./config/env.js";
-import path from "path"
+import path from "path";
 import express from "express";
 import cors from "cors";
 import quizRoutes from "./routes/quizRoutes.js";
 import { handler } from "./middlewear/route-middlewear.js";
-import { fileURLToPath } from 'url';
-
-
+import { fileURLToPath } from "url";
 
 const app = express();
-// const PORT = process.env.PORT || 3000;
 
 // Correct way to get __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -17,22 +14,26 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
+
 console.log(GENAI_API_KEY);
 
 app.use("/api/quiz", quizRoutes);
 
-// Correctly serving the frontend in production
-if(process.env.NODE_ENV === "production"){
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/build");
 
   app.use(express.static(frontendPath));
 
-  app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
+  // Fallback route for React app
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 } else {
-  app.get("/", (req, res) => res.json({ message: "Welcome to past quiz questions" }));
+  app.get("/", (req, res) =>
+    res.json({ message: "Welcome to past quiz questions" })
+  );
 }
 
 app.use(handler);
