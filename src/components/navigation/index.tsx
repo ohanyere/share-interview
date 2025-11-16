b7986d8ae0d526c2b01327c2de6c3f7e80fdeae5
Ohanyere Francis <mooref068@gmail.com>
2025-11-16 01:03:47 -0800

    fix(Navigation/index.tsx): remove unused import

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate} from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import {signOut } from "../../features/auth/authSlice";
